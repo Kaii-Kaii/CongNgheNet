eef87cdb3c6981f38521a225c5c94692516a27aa
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-10-09 18:44:17 +0700

    update

--- CongNgheNet/Form1.Designer.cs
+++ CongNgheNet/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.QuenPass = new System.Windows.Forms.LinkLabel();
+            this.DangNhap = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DangNhap);
+            this.groupBox1.Controls.Add(this.QuenPass);
+            this.groupBox1.Controls.Add(this.Password);
+            this.groupBox1.Controls.Add(this.UserName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(143, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 312);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên tài khoản:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // UserName
+            // 
+            this.UserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserName.Location = new System.Drawing.Point(165, 54);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(195, 28);
+            this.UserName.TabIndex = 2;
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(165, 100);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(197, 28);
+            this.Password.TabIndex = 3;
+            // 
+            // QuenPass
+            // 
+            this.QuenPass.AutoSize = true;
+            this.QuenPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuenPass.Location = new System.Drawing.Point(243, 141);
+            this.QuenPass.Name = "QuenPass";
+            this.QuenPass.Size = new System.Drawing.Size(117, 18);
+            this.QuenPass.TabIndex = 4;
+            this.QuenPass.TabStop = true;
+            this.QuenPass.Text = "Quên mật khẩu?";
+            // 
+            // DangNhap
+            // 
+            this.DangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DangNhap.Location = new System.Drawing.Point(204, 171);
+            this.DangNhap.Name = "DangNhap";
+            this.DangNhap.Size = new System.Drawing.Size(116, 28);
+            this.DangNhap.TabIndex = 5;
+            this.DangNhap.Text = "Đăng nhập";
+            this.DangNhap.UseVisualStyleBackColor = true;
+            this.DangNhap.Click += new System.EventHandler(this.DangNhap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button DangNhap;
+        private System.Windows.Forms.LinkLabel QuenPass;
     }
 }
 
--- CongNgheNet/Form1.cs
+++ CongNgheNet/Form1.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using CongNgheNet;
+using System.Data.SqlClient;
+using System.Configuration;
 namespace CongNgheNet
 {
     public partial class Form1 : Form
     {
+        //	<connectionStrings>
+        //<add name = "conn" connectionString="Data Source=KAIIKAII;Initial Catalog=Net_user;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"/>
+        //</connectionStrings>
+        SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);    
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DangNhap_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            string tk = UserName.Text;  
+            string mk = Password.Text;
+            string sql = "select * from User_ where username = '" + tk + "' and pass = '" + mk + "'";
+            SqlCommand cmd = new SqlCommand(sql, connect);
+            SqlDataReader dta = cmd.ExecuteReader();
+            if (dta.Read() == true)
+            {
+                MessageBox.Show("Đăng nhập thành công");
+                Main f = new Main();
+                f.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+            connect.Close();
         }
     }
 }
